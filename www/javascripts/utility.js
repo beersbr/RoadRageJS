f1d2eeceafb27bacf4afa812bea0a5afaa3263cf
Brett Beers <beersbr@gmail.com>
2012-10-25 15:20:52 -0600

    Added the request anim frame to the utility file. updated the road-rage html with the loop and game states

--- www/javascripts/utility.js
+++ www/javascripts/utility.js
@@ -1,3 +1,14 @@
+requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+     window.webkitRequestAnimationFrame ||
+     window.mozRequestAnimationFrame ||
+     window.oRequestAnimationFrame ||
+     window.msRequestAnimationFrame ||
+     function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+       window.setTimeout(callback, 1000/60);
+     };
+})();
+
 Utility = {
 
 	// This assumes that the arguments is a single object (array, dictionary)

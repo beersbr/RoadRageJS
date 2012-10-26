4d6dbb105f9d2bf99aba4cdbbb8626eda382a2bb
unknown <npriego@UTLoaner02.mpifix.com>
2012-10-25 18:46:15 -0600

    web_server changes

--- ruby/web_server.rb
+++ ruby/web_server.rb
@@ -1,5 +1,33 @@
 require 'sinatra'
 
-get '/home' do 
-	"stuff"
+DocRoot = File.dirname(__FILE__)
+DocRoot = DocRoot+"/../www/styles"
+puts File.expand_path(DocRoot)
+
+set :public_folder, DocRoot
+
+
+get '/' do
+	send_file('road_rage.html')
+	# File.read(File.join('www', 'road_rage.html'))
+	# methodName = params[:splat][0].to_s
+	# params.to_s
+	# if path == 'save'
+	# 	"in save"
+	# end
 end
+
+get 'getUser' do
+	params
+end
+
+get 'setUser' do
+	params
+end
+
+get '/*' do
+
+end
+
+
+

686af2d95b454416f9231f922cf4970a15d6134a
Brett Beers <beersbr@gmail.com>
2012-10-26 05:53:29 -0600

    Made the level editor. small fixes here and there. Cleaned up the pause loop (sort of in the sense that it looks better to the view now)

--- ruby/web_server.rb
+++ ruby/web_server.rb
@@ -13,6 +13,18 @@ get '/' do
 	redirect "road_rage.html"
 end
 
+post '/saveLevel' do 
+	lvl_data = params["lvl"]
+	name = params["name"]
+
+	puts "SAVING: #{lvl_data} as #{name}"
+
+	File.open(File.join(DocRoot, "json/"+name+".json"), "w+") do |f|
+		f.write(lvl_data);
+	end
+
+end
+
 get 'getUser' do
 	params
 end
